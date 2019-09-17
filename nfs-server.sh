7410821bfefbd8aec2eb8bc2077a87c6f1cb347c
Paolo Visintin <paolo.visintin@cloud.timenet.it>
2019-09-17 23:57:01 +0200

    Add nfs-server configuration

---
+++ nfs-server.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+NODES=$($K get nodes --no-headers -o wide -o custom-columns=INTERNAL-IP:.status.addresses[0].address)
+apt-get install nfs-kernel-server
+for NODE in $NODES; 
+do
+cat << EOF >> /etc/exports
+/var/lib/cni/networks ${NODE}/32(rw,no_root_squash)
+EOF
+
+done
+
+systemctl restart nfs-kernel-server
